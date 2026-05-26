938c1d05de0652bb9724decfe23ac74634f9b3d0
Utka <vlad-307123@mail.ru>
2026-05-26 23:37:20 +0800

    Создали класс "частица"

---
+++ Particle.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp.lab6
+{
+    class Particle
+    {
+        public int Radius; // радуис частицы
+        public float X; // X координата положения частицы в пространстве
+        public float Y; // Y координата положения частицы в пространстве
+    }
+}
