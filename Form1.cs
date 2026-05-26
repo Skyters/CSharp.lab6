cbb71cc337bdf6c5481be734c54a4224e7b60603
Utka <vlad-307123@mail.ru>
2026-05-26 23:48:24 +0800

    Успешный взрыв с частицами

--- Form1.cs
+++ Form1.cs
@@ -23,28 +23,38 @@ namespace CSharp.lab6
             }
 
         }
-
-        int counter = 0; // добавлю счетчик чтобы считать вызовы функции
-        private void timer1_Tick(object sender, EventArgs e)
+        
+        private void timer1_Tick(object sender, EventArgs e) // dsdjl xfcnbws
         {
-            counter++; // увеличиваю значение счетчика каждый вызов
+
+            UpdateState(); // каждый тик обновляем систему
+
             using (var g = Graphics.FromImage(picDisplay.Image))
             {
-                g.Clear(Color.White); // добавил очистку
-                // рисую на изображении сколько насчитал
-                g.DrawString(
-                    counter.ToString(), // значения счетчика в виде строки
-                    new Font("Arial", 12), // шрифт
-                    new SolidBrush(Color.Black), // цвет
-                    new PointF
-                    { // по центру экрана
-                        X = picDisplay.Image.Width / 2,
-                        Y = picDisplay.Image.Height / 2
-                    }
-                );
+                g.Clear(Color.White);
+                Render(g); // рендерим систему
             }
 
             picDisplay.Invalidate();
+        }
+
+        
+        private void UpdateState() // обновления состояния системы
+        {
+            foreach (var particle in particles)
+            {
+                var directionInRadians = particle.Direction / 180 * Math.PI;
+                particle.X += (float)(particle.Speed * Math.Cos(directionInRadians));
+                particle.Y -= (float)(particle.Speed * Math.Sin(directionInRadians));
+            }
+        }
+
+        private void Render(Graphics g)
+        {
+            foreach (var particle in particles)
+            {
+                particle.Draw(g);
+            }
         }
     }
 }
